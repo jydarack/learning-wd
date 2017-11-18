c4d46df77961e382bffccd01dedf5f33ed0fd689
Jean YAHIAOUI <Jean@Jeans-MacBook-Air.local>
2017-11-18 23:25:19 +0900

    Added one exemple from PHP textbook

---
+++ PHP/test1.php
@@ -0,0 +1,10 @@
+<?php // Test1.php
+// From Learning PHP, CSS & Javascript (Oreilly)
+$username = "Jane Doe";
+echo $username;
+echo "<br />";
+$current_user = $username;
+echo $current_user;
+
+// after python, these semi-colon will kill me...
+ ?>
