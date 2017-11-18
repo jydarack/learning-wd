aa81cedc26b64887beb1f5545e5846f27c53d2bc
Jean YAHIAOUI <Jean@Jeans-MacBook-Air.local>
2017-11-18 22:37:10 +0900

    Pushing the few existing files into the depositery. It's alive !

---
+++ PHP/hello.php
@@ -0,0 +1,10 @@
+<HTML>
+    <head>
+        <title> Juste un petit test pour démarrer. </title>
+    </head>
+    <body>
+
+<?php
+    echo "<h2>Hello World. </h2>\nToday is ".date("l").". ";
+?>
+How are you ?
--- Random thoughts/readme.txt
+++ Random thoughts/readme.txt
@@ -2,3 +2,5 @@ Ok, this is another test.
 
 Have finally managed to clone a github directory, manage it with git.
 Creating this file to push it to github
+
+And we have a GO !
